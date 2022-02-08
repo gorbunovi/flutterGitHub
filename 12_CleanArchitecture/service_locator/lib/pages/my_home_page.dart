19d883ffc3b91160a96701f22d76eebc54f84af6
Igor <gorbunovi@rambler.ru>
2022-02-08 14:35:55 +0400

    --

--- 12_CleanArchitecture/service_locator/lib/pages/my_home_page.dart
+++ 12_CleanArchitecture/service_locator/lib/pages/my_home_page.dart
@@ -1,6 +1,4 @@
 
-import 'dart:developer';
-
 import 'package:buisnece_packege/buisnece_packege.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
--- 12_CleanArchitecture/service_locator/packages/buisnece_packege/lib/src/counter_bloc.dart
+++ 12_CleanArchitecture/service_locator/packages/buisnece_packege/lib/src/counter_bloc.dart
@@ -8,11 +8,16 @@ abstract class CounterEvent {}
 class CounterIncrement extends CounterEvent {}
 class CounterDecrement extends CounterEvent {}
 
-
 class CounterBloc extends Bloc<CounterEvent, Counter>{
 
   CounterBloc() : super(ServiceProvider.instance.get<Counter>()){
-    on<CounterIncrement>(event, emit) => emit(state.updateCounter(state.counter + 1));
-    on<CounterDecrementt>(event, emit) => emit(state.updateCounter(state.counter - 1));
+    on<CounterIncrement>((event, emit){
+      state.updateCounter(state.counter + 1);
+      emit(state);
+    });
+    on<CounterDecrement>((event, emit){
+      state.updateCounter(state.counter + 1);
+      emit(state);
+    });
   }
 }
